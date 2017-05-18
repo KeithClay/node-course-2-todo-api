fa868a60c1dc1dc87423d51c98f491283b345374
Keith <keithclay59@gmail.com>
2017-05-17 18:19:04 -0600

    Added POST /todos route and refactor mongoose.

---
+++ server/models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+// var user = new User({
+//   email: 'keithclay59@gmail.com    '
+// });
+//  user.save().then ((doc) => {
+//    console.log(JSON.stringify(doc, undefined, 2));
+//    }, (e) => {
+//    console.log('Unable to save Todo', e)
+//    });
+
+module.exports = {User};
