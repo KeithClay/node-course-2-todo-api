ed1bbf23ddde1b821830b1ec3ba79219f3d0499c
Keith <keithclay59@gmail.com>
2017-05-21 14:11:08 -0600

    Add DELETE todos/:id route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ var {User} = require('./models/user');
 var app = express();
 const port = process.env.PORT || 3000;
 
+
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -48,6 +49,25 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    } 
+      res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
